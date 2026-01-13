79b49f59e6b7c965c234e05acfd02a5c405dcd85
Mohammed Naqhid <mnaqhid@gmail.com>
2026-01-13 15:13:32 +0530

    fix 404 error in live deploy link

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const logger = require('./utils/logger');
 const fileWatcherService = require('./services/fileWatcherService');
 const apiController = require('./controllers/apiController');
+const testController = require('./controllers/testController');
 const errorHandler = require('./middleware/errorHandler');
 
 // Create required directories
