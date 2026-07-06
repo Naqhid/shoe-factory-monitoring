5e39d0b757537c01d44dd5822dbc4c83e6a8a2e0
Naqhid <mnaqhid@gmail.com>
2026-07-06 17:45:00 +0530

    chore: DB backup backup-florence-2026-07-06T12-15-00.sql - 7/6/2026, 5:45:00 PM

--- production-tracker.bat
+++ production-tracker.bat
@@ -3,9 +3,6 @@
 :: Node/NPM paths
 SET NPM="C:\Program Files\nodejs\npm.cmd"
 
-:: Start Frontend
-cd /d "D:\Software Projects\shoe-factory-monitoring\frontend"
-start "Frontend" %NPM% run dev
 
 :: Start Backend
 cd /d "D:\Software Projects\shoe-factory-monitoring\backend"
